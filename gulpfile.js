ec532788ad61aa537cd9ef94b119fb8ca4b1feec
Aleksei <eastonsc6@yandex.ru>
2020-11-07 14:45:59 +0300

    complete pages

--- gulpfile.js
+++ gulpfile.js
@@ -51,7 +51,7 @@ function styles() {
 function scripts() {
     return gulp.src([
         'node_modules/jquery/dist/jquery.min.js',
-        'node_modules/slick-carousel/slick/slick.js',
+        'node_modules/slick-carousel/slick/slick.min.js',
         'node_modules/wow.js/dist/wow.js',
         'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.min.js',
         jsDir
--- src/js/index.js
+++ src/js/index.js
@@ -2,10 +2,19 @@ $(function () {
     /*new WOW({
         animateClass: 'animate__animated'
     }).init();*/
-    $('[data-fancybox]').fancybox({
-        youtube: {
-            controls: 0,
-            showinfo: 0
-        }
+
+    $('.heroes__slider-img').slick({
+        asNavFor: '.heroes__slider-text',
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: '<button type="button" class="slick-prev slick-btn"><img src="images/arrow-prev.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-next slick-btn"><img src="images/arrow-next.svg" alt=""></button>'
     });
+    $('.heroes__slider-text').slick({
+        asNavFor: '.heroes__slider-img',
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        arrows: false
+    })
 });
