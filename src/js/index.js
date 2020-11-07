92aed298ec2bd254684f09ff3168a7e2c335e6b8
Aleksei <eastonsc6@yandex.ru>
2020-11-07 17:28:41 +0300

    complete adaptive

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,24 @@
 $(function () {
-    /*new WOW({
+    new WOW({
         animateClass: 'animate__animated'
-    }).init();*/
+    }).init();
 
     $('.heroes__slider-img').slick({
         asNavFor: '.heroes__slider-text',
         slidesToShow: 1,
         slidesToScroll: 1,
         prevArrow: '<button type="button" class="slick-prev slick-btn"><img src="images/arrow-prev.svg" alt=""></button>',
-        nextArrow: '<button type="button" class="slick-next slick-btn"><img src="images/arrow-next.svg" alt=""></button>'
+        nextArrow: '<button type="button" class="slick-next slick-btn"><img src="images/arrow-next.svg" alt=""></button>',
+        responsive: [
+            {
+                breakpoint: 769,
+                settings: {
+                    arrows: false,
+                    autoplaySpeed: 2000,
+                    autoplay: true
+                }
+            }
+        ]
     });
     $('.heroes__slider-text').slick({
         asNavFor: '.heroes__slider-img',
@@ -16,5 +26,10 @@ $(function () {
         slidesToScroll: 1,
         fade: true,
         arrows: false
+    });
+
+    $('.menu-btn').on('click', function () {
+        $('.menu__list').toggleClass('menu__list-active');
+        $('.menu-btn').toggleClass('opened');
     })
 });
